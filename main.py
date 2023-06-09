1d22cac6d284b06de10c9f939ce568e378904d16
barmi <bar.milman@mail.huji.ac.il>
2023-06-09 03:35:26 +0300

    dahy 22222

--- main.py
+++ main.py
@@ -6,7 +6,41 @@ from classification import Classification
 from data_handler import load_data, split_data
 from regression import Regression
 
+
+def temp():
+    file_path = './data_files/agoda_cancellation_train.csv'
+
+    # Order Cancelation
+    df_cancel = load_data(file_path, True)
+    df_cancel = task_1.preprocess_data(df_cancel, True)
+    train_df, test_df = split_data(df_cancel)
+    train_df = train_df.drop_duplicates()
+
+    X_Train = train_df.loc[:, ~train_df.columns.isin(["order_cancelled"])]
+    y_Train = train_df["order_cancelled"]
+    X_Test = test_df.loc[:, ~test_df.columns.isin(["order_cancelled"])]
+    y_Test = test_df["order_cancelled"]
+
+    Classification().run_all(X_Train, y_Train, X_Test, y_Test)
+
+    # Price
+    df_price = load_data(file_path, True)
+
+    df_price = task_2.preprocess_data(df_price, True)
+    train_df, test_df = split_data(df_price)
+    train_df = train_df.drop_duplicates()
+
+    X_Train = train_df.loc[:, ~train_df.columns.isin(["original_selling_amount"])]
+    y_Train = train_df["original_selling_amount"]
+    X_Test = test_df.loc[:, ~test_df.columns.isin(["original_selling_amount"])]
+    y_Test = test_df["original_selling_amount"]
+
+    Regression().run_all(X_Train, y_Train, X_Test, y_Test)
+
+
 if __name__ == "__main__":
+    temp()
+
     train_path = './data_files/agoda_cancellation_train.csv'
 
     # Order Cancelation
